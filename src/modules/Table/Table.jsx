50b803dd278a7aede8813eb96d617fa6a96deb15
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-10-25 17:16:15 +0300

    add styles

--- src/modules/Table/Table.jsx
+++ src/modules/Table/Table.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-
 import { gettableData, deleteTableDataEl } from "../../shared/api/table";
+
+import styles from "./table.module.scss";
 
 const Table = () => {
 	const [tableData, setTableData] = useState([]);
-	console.log(tableData);
 	const [deletedId, setDeletedId] = useState(null);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(null);
@@ -39,28 +39,47 @@ const Table = () => {
 		}
 	}, [deletedId]);
 
-	const elements =
-		tableData &&
-		tableData.map(
-			({ id, name, email, birthday_date, phone_number, address }) => (
-				<li key={id}>
-					{name}
-					{email}
-					{birthday_date}
-					{phone_number}
-					{address}
-					<button onClick={() => setDeletedId(id)} type="button">
-						Delete
-					</button>
-				</li>
-			),
+	let content;
+
+	if (loading) {
+		content = <p>Loading...</p>;
+	} else if (error) {
+		content = <p className={styles.error}>{error}</p>;
+	} else {
+		content = (
+			<tb className={styles.tb}>
+				{tableData.results &&
+					tableData.results.map(
+						({ id, name, email, birthday_date, phone_number, address }) => (
+							<tr key={id} className={styles.table}>
+								<td className={styles.td}>{name}</td>
+								<td className={styles.td}>{email}</td>
+								<td className={styles.td}>{birthday_date}</td>
+								<td className={styles.td}>{phone_number}</td>
+								<td className={styles.td}>{address}</td>
+								<button onClick={() => setDeletedId(id)} type="button">
+									Delete
+								</button>
+								<button type="button">Add</button>
+							</tr>
+						),
+					)}
+			</tb>
 		);
+	}
 
 	return (
 		<>
-			{loading && <p>....Loading</p>}
-			{error && <p className={styles.error}>{error}</p>}
-			<ol>{elements}</ol>
+			<thead className={styles.thead}>
+				<tr className={styles.tableRaw}>
+					<th className={styles.th}>Name</th>
+					<th className={styles.th}>Email</th>
+					<th className={styles.th}>Birthday_date</th>
+					<th className={styles.th}>Phone_number</th>
+					<th className={styles.th}>Adress</th>
+				</tr>
+			</thead>
+			<div>{content}</div>
 		</>
 	);
 };
--- src/shared/api/auth.js
+++ src/shared/api/auth.js
@@ -4,7 +4,7 @@ const instance = axios.create({
 	baseURL: "https://technical-task-api.icapgroupgmbh.com/api/",
 });
 
-export const login = async (data) => {
-	const { data: result } = await instance.post("/login/", data);
+export const login = (data) => {
+	const { data: result } = instance.post("/login/", data);
 	return result;
 };
