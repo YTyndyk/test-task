820e6b0cefce396eda11a243a02595f74df6cb92
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-10-26 10:31:39 +0300

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -18,8 +18,6 @@ const LoginPage = () => {
 					className={styles.image}
 					src="https://img2.joyreactor.cc/pics/post/%D1%82%D0%B0%D0%B1%D0%BB%D0%B8%D1%86%D1%8B-%D0%BC%D0%BE%D0%BD%D1%81%D1%82%D1%80%D1%8B-%D1%81%D0%B4%D0%B5%D0%BB%D0%B0%D0%B9-%D1%81%D0%B0%D0%BC-138920.jpeg"
 					alt="photo table"
-					width={300}
-					height={300}
 				/>
 			</div>
 		</div>
